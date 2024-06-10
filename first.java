37e3425753edb1914f2abd45f60cc38957c3598c
aadityasaini2004 <adityasaini472894@gmail.com>
2024-06-10 15:29:55 +0530

    Solid Rectangles With Stars using java

--- first.java
+++ first.java
@@ -1,6 +1,15 @@
 public class first {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int n = 4;
+        int m = 5;
+
+        // mainLoop:
+        for (int i=0; i<=n; i++) {
+            // inner loop
+            for (int j=0; j<=m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
